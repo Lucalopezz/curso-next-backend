d60dab685ada0e4387f0f72114bfc4fc6b78d8ef
Lucas <lucaslops12@gmail.com>
2026-01-05 17:07:20 -0300

    feat(user): implement User entity, service, and module with TypeORM integration

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class UserEntity {}
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  forceLogout: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
